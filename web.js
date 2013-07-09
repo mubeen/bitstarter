c4b2b2da630cde54d193cf75673766ffb3238173
Mubeen Ahmad <mubeen@fingerprintsoft.co>
2013-07-09 20:43:17 +0000

    added index.html text

--- web.js
+++ web.js
@@ -2,10 +2,9 @@ var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-Buffer buffer = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  response.send(fs.readFileSync('index.html').toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
